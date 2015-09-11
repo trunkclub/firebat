3ac60b38c21334c16b774f29445dde749a5759bb
Jeff Meyers <jeffreym.meyers@gmail.com>
2015-09-11 14:41:11 -0500

    TIL module_function is the worst

--- lib/flare.rb
+++ lib/flare.rb
@@ -2,16 +2,5 @@ require 'config/load'
 require 'modules/logs_messages'
 
 module Flare
-  include LogsMessages
-  @@_logger = Logger.new
-
-  def logger=(logger)
-    @@_logger = logger
-  end
-  module_function :logger=
-
-  def log(message)
-    @@_logger.log(message)
-  end
-  module_function :log
+  extend LogsMessages
 end
--- lib/modules/logs_messages.rb
+++ lib/modules/logs_messages.rb
@@ -1,2 +1,13 @@
 module LogsMessages
+  extend self
+
+  @@_logger = Logger.new
+
+  def logger=(logger)
+    @@_logger = logger
+  end
+
+  def log(message)
+    @@_logger.log(message)
+  end
 end
